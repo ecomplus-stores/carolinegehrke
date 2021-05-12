e9b6f40455681dff506ca736606464abefa79707
eduvlemes <edu.vlemes@gmail.com>
2021-05-12 02:13:31 -0300

    ajustes formularios

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -41,8 +41,54 @@ const apx = {
     updateDiscount : function(oObj){
         let discountPrice =  parseFloat(parseFloat(oObj.text().trim().replace('R$','').replace('.','').replace(',','.').replace(' ','')).toFixed(2)) * atacado[0];
         oObj.next('.apx-discount__rule').html(ecomUtils.formatMoney(discountPrice)+' no <b>ATACADO</b>');
-    }
+    }    
 };
+
+$('.apx_newsletter form').submit(function(e){
+    e.preventDefault();
+    var news = [];
+    news.form = $(this);
+    news.mail = $(this).find('input[name="email"]').val();
+    news.name = $(this).find('input[name="name"]').val();
+    axios.post('https://us-central1-marketingtools-ecomplus.cloudfunctions.net/app/alpix/apx_newsletter', {
+        storeId : storefront.settings.store_id,
+        mail : news.mail,
+        fullname : news.name
+    })
+    .then(function(response){
+        alert(response.data.msg)
+        if(!response.data.error){
+            news.form.find('input[type="text"],input[type="email"]').val('')
+        }
+    })
+});
+
+$('.apx_form').submit(function(e){
+    e.preventDefault();
+    var mail = [];
+    mail.form = $(this);
+    mail.destination = $(this).find('input[name="destination"]').val();
+    mail.subject = $(this).find('input[name="subject"]').val();
+    mail.body = "";
+
+    mail.form.find('input, textarea').each(function(){
+        mail.body = mail.body + $(this).closest('div').find('label').text() + ': ' + $(this).val();
+    });
+
+    axios.post('https://us-central1-marketingtools-ecomplus.cloudfunctions.net/app/alpix/apx_sendmail', {
+        storeId : storefront.settings.store_id,
+        destination : mail.destination,
+        subject : mail.subject,
+        content : mail.body
+    })
+    .then(function(response){
+        alert(response.data.msg)
+        if(!response.data.error){
+            news.form.find('input[type="text"],input[type="email"],textarea,input[type="tel"]').val('')
+        }
+    })
+    
+});
 
 $(document).ready(function(){
     apx.imageProportion();
@@ -58,8 +104,8 @@ $(document).ready(function(){
             const me = $(this).find('.row');
             ecomClient.store({ url: '/products/'+ $(this).data('look-id') + '.json' }).then(({ data }) => { 
                 
-                $('<div class="col-6 kit-cover pr-0"><a href="'+ data.slug +'"><picture><img data-src="'+ data.pictures[0].zoom.url +'" alt="'+ data.name+'" class="lozad-delay fade" crossorigin="anonymous"/></picture></a></div>').appendTo(me);
-                $('<div class="col-6 kit-items"></div>').appendTo(me);
+                $('<div class="col-md-6 col-12 kit-cover pr-md-0 pb-md-0 pb-4"><a href="'+ data.slug +'"><picture><img data-src="'+ data.pictures[0].zoom.url +'" alt="'+ data.name+'" class="lozad-delay fade" crossorigin="anonymous"/></picture></a></div>').appendTo(me);
+                $('<div class="col-md-6 col-12 kit-items"></div>').appendTo(me);
 
                 const items_id = [];
                 $.each(data.kit_composition,function(a,b){
@@ -72,7 +118,7 @@ $(document).ready(function(){
                         let mb = result.hits.hits.length - 1 != k ? 'mb-3' : '' 
                         let r = $('<div class="row align-items-center '+ mb +'"></div>');
                         let parcelas = 3;
-                        r.append('<div class="col-5"><picture><img data-src="'+ product.pictures[0].zoom.url +'" alt="'+ product.name+'" class="lozad-delay fade" crossorigin="anonymous"/></picture></div>');
+                        r.append('<div class="col-5 pr-0 pr-md-3"><picture><img data-src="'+ product.pictures[0].zoom.url +'" alt="'+ product.name+'" class="lozad-delay fade" crossorigin="anonymous"/></picture></div>');
                         r.append('<div class="col-7"><strong>'+ product.name +'</strong><span><strong>' + ecomUtils.formatMoney(product.price) + '</strong> ou <span> '+ parcelas +' de '+ecomUtils.formatMoney(product.price / parcelas)+'</span></span><small>'+ ecomUtils.formatMoney((product.price * atacado[0])) +' no <b>ATACADO</b></small></div>')
                         r.appendTo(me.find('.kit-items'));
                     });
@@ -88,7 +134,7 @@ $(document).ready(function(){
                     
             }).catch(console.error)
         })       
-    }
+    }   
     
 })
 
